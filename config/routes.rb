ab78ff1733c5e5e2600fbe4acb1bf4287ed640e2
claire <clairebontempo@gmail.com>
2021-01-10 12:17:50 -0800

    commit tree a82a57a985054f3a5c447af6828081d4b02f3f86

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:show, :index] do #gives us access to authors/:author_id/posts/new
+    resources :posts, only: [:show, :index, :new, :edit] #and new_author_post_pathn 
+  end
+
+  resources :posts 
 end
